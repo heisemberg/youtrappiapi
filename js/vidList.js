7b9368f1c4736218452b25c215897b21d28cae20
Heisemberg David Soler <heissen1122@gmail.com>
2023-08-29 11:33:42 -0500

    refactor: :recycle: sonsulta api lista videos
    se optimiza peticion a los 2 archivos json con validacion de errores

--- js/vidList.js
+++ js/vidList.js
@@ -1,26 +1,32 @@
+export const vidList = async() => {
+    try {
+        const [videosResponse, detailsResponse] = await Promise.all([
+            fetch("../json/channelVideos.json"),
+            fetch("../json/detailsVideos.json")
+        ]);
 
-export let vidList = async() =>{
-    let peticion = await fetch("../json/channelVideos.json");
-    let res1 = await peticion.json();
-    let peticion2 = await fetch("../json/detailsVideos.json");
-    let res2 = await peticion2.json();
-  
-    let videosList = document.querySelector(".list-container");
-    videosList.insertAdjacentHTML("beforeend", /* html */`
-    ${res1.contents.map((value)=> /*html*/ `   
-    <div class="vid-list">
-        <a href="play-video.html"><img class="thumbnail" src="${value.video.thumbnails[3].url}" alt=""></a>
-        <div class="flex-div">
-            <img src="${res2.avatar[0].url}" alt="">
-            <div class="vid-info">
-                <a href="play-video.html">${value.video.title}</a>
-                <p>${res2.title}</p>
-                <p>${value.video.stats.views} Views &bull; ${value.video.publishedTimeText}</p>
+        const videosData = await videosResponse.json();
+        const detailsData = await detailsResponse.json();
+
+        const videosList = document.querySelector(".list-container");
+        videosList.insertAdjacentHTML("beforeend", /* html */`
+        ${videosData.contents.map((value)=> /*html*/ `   
+        <div class="vid-list">
+            <a href="play-video.html"><img class="thumbnail" src="${value.video.thumbnails[3].url}" alt=""></a>
+            <div class="flex-div">
+                <img src="${detailsData.avatar[0].url}" alt="">
+                <div class="vid-info">
+                    <a href="play-video.html">${value.video.title}</a>
+                    <p>${detailsData.title}</p>
+                    <p>${value.video.stats.views} Views &bull; ${value.video.publishedTimeText}</p>
+                </div>
             </div>
         </div>
-    </div>
-    `).join("")}
-    `)
+        `).join("")}
+        `)
+    }
+  
+    catch (error) {
+        console.error("Error al cargar los datos:", error);
+    }
 }
-  
-  
