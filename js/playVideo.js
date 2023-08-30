e9ea387c5e189ea3248b3d2dd2190c7f37c19a4b
heisemberg <heissen1122@gmail.com>
2023-08-29 20:21:46 -0500

    refactor::recycle: js reproductor video
    actualizacion reproducion video configuracion del reproductor

--- js/playVideo.js
+++ js/playVideo.js
@@ -4,19 +4,35 @@ export const playVideo = async() => {
             fetch("../json/channelVideos.json"),
             fetch("../json/detailsVideos.json")
         ]);
-
+        const idVideo = localStorage.getItem("ID");
         const videosData = await videosResponse.json();
         const detailsData = await detailsResponse.json();
 
         const videoPlay = document.querySelector(".play-video");
         videoPlay.insertAdjacentHTML("beforeend", /* html */`
-        <video controls autoplay>
-            <source src="images/video.mp4" type="video/mp4">
-        </video>
+        <iframe height="700px" width="100%" src="https://www.youtube.com/embed/${idVideo}" frameborder="0" allowfullscreen></iframe>
         `)
+        const videoSidebar = document.querySelector(".right-sidebar");
+        videoSidebar.insertAdjacentHTML("beforeend", /* html */`
+        <div class="side-video-list">
+            <a href="" class="small-thumbnail"><img src="images/thumbnail1.png"></a>
+            <div class="vid-info">
+                <a href="">Best channel that help you to be a web developer</a>
+                <p>Easy Tutorials</p>
+                <p>15k Views</p>
+            </div>
+        </div>
+        `)   
     }
   
     catch (error) {
         console.error("Error al cargar los datos:", error);
     }
 }
+
+
+
+
+ 
+
+  
